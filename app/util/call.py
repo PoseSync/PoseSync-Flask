3675177293b69576f1281c723fced01a729fac86
ksj321456 <ksj321456@gmail.com>
2025-05-11 18:33:13 +0900

    낙상감지 후 전화걸기 추가

---
+++ app/util/call.py
@@ -0,0 +1,25 @@
+from twilio.rest import Client
+
+# Twilio 계정 정보
+account_sid = 'ACf8776942e731c676dc51827a6d5ffa84'
+auth_token = '957b48200da6aa7be0ae674051a4a2d7'
+twilio_number = '+12694212923'  # 너가 구매한 번호
+
+# 추후 수정
+voice_url = 'http://<EC2-IP>/voice.xml'  # AWS에서 실행 중인 XML 주소
+
+client = Client(account_sid, auth_token)
+
+def call_user():
+    try:
+        call = client.calls.create(
+            # 수신자 번호(성재 번호)
+            to=+821062538642,
+            # twilio에서 발급받은 전화번호
+            from_=twilio_number,
+            # xml
+            url=voice_url
+        )
+        print(f"☎️ 전화 발신 완료! Call SID: {call.sid}")
+    except Exception as e:
+        print(f"❌ 전화 발신 중 오류: {e}")
