16f2cdc948e7de29d001c2263dfe44134a24f189
ksj321456 <ksj321456@gmail.com>
2025-05-12 16:59:48 +0900

    Entity 수정

--- app/models/exercise_result.py
+++ app/models/exercise_result.py
@@ -12,5 +12,9 @@ class ExerciseResult(db.Model):
     # date = db.Column(db.DateTime, default=datetime.now(UTC), nullable=False)  # 현재 날짜 기본값
     date = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)  # UTC 시간을 올바르게 설정
     exercise_name = db.Column(db.String(50), nullable=False)
-    count = db.Column(db.Integer, nullable=False)
+
+    # 목표 횟수
+    target_count = db.Column(db.Integer, nullable=False)
+    # 현재 횟수
+    currnet_count = db.Column(db.Integer, nullable=False)
     weight = db.Column(db.Float, nullable=False)  # 무게는 선택사항으로 nullable=True -> # 2025/04/27 무게는 0도 입력받아야 하므로 False로 설정
