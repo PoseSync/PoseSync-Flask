50c80c2414569169f289310d04852be7258f6efe
HSSJW <okqkfkfk@naver.com>
2025-05-25 19:11:58 +0900

    Feat : 카운터 코드 및 global_state.py 수정

--- app/services/sholderPressService/sholderPressService.py
+++ app/services/sholderPressService/sholderPressService.py
@@ -4,7 +4,7 @@ from app.util.pose_landmark_enum import PoseLandmark
 from app.util.exercise_util.shoulderPress_util import calculate_elbow_position_by_forward_angle, \
     adjust_wrist_direction_to_preserve_min_angle
 # 공유 전역 상태에서 body_type과 뼈길이 데이터 가져오기
-from app.shared.global_state import counter
+import app.shared.global_state as gs
 
 
 def process_dumbbell_shoulderPress(data):
@@ -140,18 +140,20 @@ def process_dumbbell_shoulderPress(data):
 
     # 운동 한 회가 완료되면 카운트 증가 --------------------------------------
     # press_counter가 None이 아닌 경우에만 카운팅 로직 실행
-    if counter:
+    print(f'❌ 카운터 시작 {gs.counter}')
+
+    if gs.counter:
         # 왼팔 기준으로 운동 횟수 업데이트
-        completed = counter.update(landmarks)
+        completed = gs.counter.update(landmarks)
 
         # 운동 한 회가 완료되면 카운트 증가
         if completed:
-            count = counter.count
+            count = gs.counter.count
             print(f"✅ 운동 횟수 변경: {count}회")
             data["count"] = count
         elif "count" not in data:
             # 이전 카운트 값이 없으면 현재 카운트 추가
-            data["count"] = counter.count
+            data["count"] = gs.counter.count
         # 운동 한 회가 완료되면 카운트 증가 ------------------------------------
 
 
